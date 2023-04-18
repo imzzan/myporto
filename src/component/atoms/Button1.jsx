2b585cf8d03309ebc6b243c527a31f8f145de7ed
TugasKu23 <kutugas23@gmail.com>
2023-04-18 20:43:44 +0700

    fix image

--- src/component/atoms/Button1.jsx
+++ src/component/atoms/Button1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button1 = ({name, className}) => {
   return (
-    <button className={` bg-blue w-fit py-1 px-5 text-sm lg:text-[16px] text-white rounded-2xl ${className}`}>{name}</button>
+    <button className={` bg-blue dark:bg-darkblue border border-white w-fit py-1 px-5 text-sm md:text-[16px] lg:text-[20px] text-white rounded-2xl ${className}`}>{name}</button>
   )
 }
 
