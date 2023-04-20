6110f382c8c37c02a3bfb66d3622a59789b36224
TugasKu23 <kutugas23@gmail.com>
2023-04-20 17:12:51 +0700

    add see more

--- src/component/atoms/Button1.jsx
+++ src/component/atoms/Button1.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button1 = ({name, className}) => {
+const Button1 = ({name, className, onclick}) => {
   return (
-    <button className={` bg-blue dark:bg-darkblue border border-white w-fit py-1 px-5 text-[16px] md:text-[18px] text-white rounded-2xl ${className}`}>{name}</button>
+    <button onClick={onclick} className={` bg-blue dark:bg-darkblue border border-white w-fit py-1 px-5 text-[16px] md:text-[18px] text-white rounded-2xl ${className}`}>{name}</button>
   )
 }
 
--- src/component/section/Project.jsx
+++ src/component/section/Project.jsx
@@ -1,28 +1,85 @@
-import React from 'react'
-import { project } from '../../constants'
+import React, { useState } from "react";
+import { project } from "../../constants";
+import {Button1} from './../atoms';
 
 const Project = () => {
-  return (
-    <div className=' py-5 dark:bg-[#222222] md:py-14 lg:py-16 px-[35px] xl:px-20' id='project'>
-      <h1 className=' dark:text-white font-bold text-[20px] text-center text-black'>Project</h1>
-      <div className=' w-4 h-4  bg-gradient-to-t from-[#2157F2] to-[#A6BCFA] rounded-full mx-auto'></div>
-      <div className=' grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 justify-center gap-5 gap-y-10'>
-        {
-          project.map((item, i) => {
-            return (
-              <div className='h-auto bg-white shadow-xl rounded-xl border border-blue' data-aos={item.aos} data-aos-delay={item.delay}>
-                <img src={item.image} alt="image" className=' h-[100px] md:h-[150px] w-full object-cover rounded-t-xl'/>
-                <div className='mt-1 p-3 flex flex-col justify-between gap-5'>
-                  <h1 className=' font-bold text-[18px] lg:text-[20px] text-black'>{item.name}</h1>
-                  <a href={item.link} target='_blank' className='text-[16px] md:text-sm lg:text-[16px] text-blue underline'>See Preview</a>
-                </div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
-  )
-}
+  const [hasil, setHasil] = useState(true);
 
-export default Project
+  const seeMore = () => {
+    setHasil(!hasil)
+  }
+
+  return (
+    <div
+      className=" py-5 dark:bg-[#222222] md:py-14 lg:py-16 px-[35px] xl:px-20"
+      id="project"
+    >
+      <h1 className=" dark:text-white font-bold text-[20px] text-center text-black">
+        Project
+      </h1>
+      <div className=" w-4 h-4  bg-gradient-to-t from-[#2157F2] to-[#A6BCFA] rounded-full mx-auto"></div>
+      <div className=" grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 justify-center gap-5 gap-y-10">
+        {hasil
+          ? project.map((item, i) => {
+              if (i < 8) {
+                return (
+                  <div
+                    className="h-auto bg-white shadow-xl rounded-xl border border-blue"
+                    data-aos={item.aos}
+                    data-aos-delay={item.delay}
+                  >
+                    <img
+                      src={item.image}
+                      alt="image"
+                      className=" h-[100px] md:h-[150px] w-full object-cover rounded-t-xl"
+                    />
+                    <div className="mt-1 p-3 flex flex-col justify-between gap-5">
+                      <h1 className=" font-bold text-[18px] lg:text-[20px] text-black">
+                        {item.name}
+                      </h1>
+                      <a
+                        href={item.link}
+                        target="_blank"
+                        className="text-[16px] md:text-sm lg:text-[16px] text-blue underline"
+                      >
+                        See Preview
+                      </a>
+                    </div>
+                  </div>
+                );
+              }
+            })
+          : project.map((item, i) => {
+              return (
+                <div
+                  className="h-auto bg-white shadow-xl rounded-xl border border-blue"
+                  data-aos={item.aos}
+                  data-aos-delay={item.delay}
+                >
+                  <img
+                    src={item.image}
+                    alt="image"
+                    className=" h-[100px] md:h-[150px] w-full object-cover rounded-t-xl"
+                  />
+                  <div className="mt-1 p-3 flex flex-col justify-between gap-5">
+                    <h1 className=" font-bold text-[18px] lg:text-[20px] text-black">
+                      {item.name}
+                    </h1>
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      className="text-[16px] md:text-sm lg:text-[16px] text-blue underline"
+                    >
+                      See Preview
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+      </div>
+      <Button1 name={hasil ? 'See More ' : 'See Less'} onclick={seeMore} className='mt-10'/>
+    </div>
+  );
+};
+
+export default Project;
