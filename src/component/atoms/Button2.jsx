032447cd28df275c6ed9dff430d785fec5428e30
TugasKu23 <kutugas23@gmail.com>
2023-04-20 10:20:35 +0700

    delete underline

--- src/component/atoms/Button2.jsx
+++ src/component/atoms/Button2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button2 = ({name, onclick}) => {
   return (
-    <button onClick={onclick} className=' text-blue font-[600] text-[16px] md:text-[18px] p-2 rounded-2xl underline'>{name}</button>
+    <button onClick={onclick} className=' text-blue font-[600] text-[16px] md:text-[18px] p-2 rounded-2xl'>{name}</button>
   )
 }
 
