49462d2efffdb823143f781f38314728a111b2f0
TugasKu23 <kutugas23@gmail.com>
2023-04-20 23:42:39 +0700

    fied

--- src/component/atoms/Button1.jsx
+++ src/component/atoms/Button1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button1 = ({name, className, onclick}) => {
   return (
-    <button onClick={onclick} className={` bg-blue dark:bg-darkblue border border-white w-fit py-1 px-5 text-[16px] md:text-[18px] text-white rounded-2xl ${className}`}>{name}</button>
+    <button onClick={onclick} className={` bg-blue dark:bg-darkblue border border-white w-fit py-1 px-5 text-[16px] md:text-[18px] text-white rounded-xl ${className}`}>{name}</button>
   )
 }
 
