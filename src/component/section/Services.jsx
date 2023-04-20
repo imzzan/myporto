76759d269c6cc7bb70c97780cffc7eef3f792971
TugasKu23 <kutugas23@gmail.com>
2023-04-20 23:48:38 +0700

    fixed

--- src/component/section/Services.jsx
+++ src/component/section/Services.jsx
@@ -10,7 +10,7 @@ const Services = () => {
         {
             services.map((item, i) => {
                 return (
-                    <div className=' border border-blue text-center p-3 rounded-xl' data-aos={item.aos}>
+                    <div className='rounded-lg text-center py-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700' data-aos={item.aos}>
                         <img src={item.image} alt="image" className=' w-[35px] mx-auto mb-3'/>
                         <h1 className=' font-[500] text-sm dark:text-white lg:text-[16px] text-black'>{item.name}</h1>
                     </div>
