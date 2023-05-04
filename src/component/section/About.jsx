7dccb32de8994ae3d6acd6b2cbe95eaf17ef0d7b
TugasKu23 <kutugas23@gmail.com>
2023-05-04 22:15:21 +0700

    fixed

--- src/component/section/About.jsx
+++ src/component/section/About.jsx
@@ -6,7 +6,7 @@ import { aboutme } from '../../constants';
 
 const About = () => {
   return (
-    <div className=' bg-primary dark:bg-darkblue w-full lg:h-screen mx-auto pt-5 md:py-12 lg:py-14 relative xl:px-20 -z-20' id='about'>
+    <div className=' bg-primary dark:bg-darkblue w-full h-[450px] lg:h-screen mx-auto pt-5 md:py-12 lg:py-14 relative xl:px-20 -z-20' id='about'>
       <br/>
         <div className='rounded-tl-[53px] lg:mt-20 lg:ml-0 rounded-br-[53px] backdrop-blur-xl cardabout mx-[35px] p-3 md:p-6 lg:w-[450px] lg:h-[350px]' data-aos="fade-right">
             <h1 className=' font-bold text-3xl md:text-4xl text-black mt-3 dark:text-white'>I’m <span className=' text-blue dark:text-darkblue'>Muhamad</span> Muzani</h1>
